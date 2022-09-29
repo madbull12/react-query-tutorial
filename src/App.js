d2a294ee08ec0aaa95feba06e94c59d53c5ad22e
Andrian Lysander <huangandrian02@gmail.com>
2022-09-29 08:48:07 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import "./styles.css";
 import { useQuery } from "@tanstack/react-query";
 
 export default function App() {
+  const getTodos = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/");
+    const data = await res.json();
+
+    return data;
+  };
   const { data, isError, isLoading, isFetching, isSuccess } = useQuery(
     "todo",
     getTodos
   );
-  return (
-    <div className="App">
-      <h1>Hello CoddsdeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+
+  return <div className="App">{JSON.stringify(data)}</div>;
 }
